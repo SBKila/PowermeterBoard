5386da2a4c5ff01dd22c8941dfa495ae18fd2895
Stephane Blanc <sblanckila@gmail.com>
2023-08-29 08:42:27 +0200

    move ui to form field conttainer

--- data/pmb/index.js
+++ data/pmb/index.js
@@ -1,6 +1,13 @@
 var isSettingsDirty = false;
 var pmSettings = [];
 
+$("#setMQTT").on('pagebeforeshow', function() {
+  $("#mqtt_url").val($("#mqtturl").val());
+  $("#mqtt_port").val($("#mqtturl").val().substring($("#mqtturl").val().lastIndexOf(':')));
+  $("#mqtt_login").val($("#mqttlogin").val());
+  $("#mqtt_pwd").val($("#mqttpwd").val());
+})
+
 $("#setMQTT").on("pagecreate", function () {
   $("#setMQTTForm").validate({
     rules: {
@@ -16,23 +23,26 @@ $("#setMQTT").on("pagecreate", function () {
     },
     submitHandler: function (form) {
       const formData = new FormData(form);
+      $("#mqtturl").val("mqtt://"+formData.mqtt_url+":"+formData.mqtt_port);
+      $("#mqttlogin").val(formData.mqtt_login);
+      $("#mqttpwd").val(formData.mqtt_pwd);
+      
 
-      $.ajax({
-        type: "POST",
-        url: "./mqtt",
-        data: JSON.stringify(Object.fromEntries(formData)),
-        contentType: "application/json",
-        dataType: "json",
-        success: function (data, textStatus, jqXHR) {
-          _appendNewPowermeterDisplay(data);
-          $("#powermeterslist").collapsibleset('refresh');
-          $.mobile.changePage("#landing");
-        },
-        error: function (data, textStatus, jqXHR) {
-          //process error msg
-          $.mobile.changePage("#landing");
-        }
-      });
+      
+      // $.ajax({
+      //   type: "POST",
+      //   url: "./mqtt",
+      //   data: JSON.stringify(Object.fromEntries(formData)),
+      //   contentType: "application/json",
+      //   dataType: "json",
+      //   success: function (data, textStatus, jqXHR) {
+          
+      //   },
+      //   error: function (data, textStatus, jqXHR) {
+      //     //process error msg
+      //     $.mobile.changePage("#landing");
+      //   }
+      // });
     }
   });
 });
@@ -169,15 +179,15 @@ $("#btn-upload").on("click", () => {
 $('body').on('click', 'a[pmedittarget]', function () {
   var pm = $(this).attr("pmedittarget");
   $("#editpm_ref").val("PM" + pm);
-  $("#editpm_name").val($("#name" + pm).text());
-  $("#editpm_ticks").val($("#ticks" + pm).text());
-  $("#editpm_nbtick").val($("#nbticks" + pm).text());
-  $("#editpm_voltage")[0].selectedIndex = $("#editpm_voltage option[value=" + $("#voltage" + pm).text() + "]").index();
-  $("#editpm_maxamp").val($("#maxamp" + pm).text());
-  $("#editpm_cumulative").val($("#cumulative" + pm).text());
+  $("#editpm_name").val($("#name" + pm).val());
+  $("#editpm_ticks").val($("#ticks" + pm).val());
+  $("#editpm_nbtick").val($("#nbticks" + pm).val());
+  $("#editpm_voltage")[0].selectedIndex = $("#editpm_voltage option[value=" + $("#voltage" + pm).val() + "]").index();
+  $("#editpm_maxamp").val($("#maxamp" + pm).val());
+  $("#editpm_cumulative").val($("#cumulative" + pm).val());
 
   $("body").pagecontainer("change", "#editPM");
-  $("#editpm_voltage").selectmenu();
+  //$("#editpm_voltage").selectmenu();
   $("select#editpm_voltage").selectmenu("refresh", true);
 
 });
@@ -221,18 +231,40 @@ jQuery.validator.addMethod(
 );
 function _buildPowermeterDisplay(element, index) {
   return '<h3> PM' + dioToPmReferenceIndex[element.dIO] + ': <span id="name' + element.dIO + '">' + element.name + '</span><span class="ui-li-count" id="cumulative' + element.dIO + '">' + element.cumulative + '</span></h3>' +
+    '<form>' +
     '<div class="ui-grid-a">' +
-    '<div class="ui-block-a">tick:<span id="ticks' + element.dIO + '">' + element.ticks + '</span></div>' +
-    '<div class="ui-block-b">nbTicksByKw:<span id="nbticks' + element.dIO + '">' + element.nbTickByKW + '</div>' +
+    '<div class="ui-block-a">'+
+    '<div class="ui-field-contain">'+
+		'<label>tick:</label>'+
+		'<input type="text" id="ticks' + element.dIO + '" value="' + element.ticks + '" disabled="disabled">'+
+		'</div>'+
+    '</div>'+
+    '<div class="ui-block-b">'+
+    '<div class="ui-field-contain">'+
+		'<label>nbTicksByKw:</label>'+
+		'<input type="text" id="nbticks' + element.dIO + '" value="' + element.nbTickByKW + '" disabled="disabled">'+
+		'</div>'+
+    '</div>'+
     '</div>' +
     '<div class="ui-grid-a">' +
-    '<div class="ui-block-a">voltage:<span id="voltage' + element.dIO + '">' + element.voltage + '</div>' +
-    '<div class="ui-block-b">maxAmp:<span id="maxamp' + element.dIO + '">' + element.maxAmp + '</div>' +
+    '<div class="ui-block-a">'+
+    '<div class="ui-field-contain">'+
+		'<label>voltage:</label>'+
+		'<input type="text" id="voltage' + element.dIO + '" value="' + element.voltage + '" disabled="disabled">'+
+		'</div>'+
+    '</div>'+
+    '<div class="ui-block-b">'+
+    '<div class="ui-field-contain">'+
+		'<label>maxAmp</label>'+
+		'<input type="text" id="nbticks' + element.dIO + '" value="' + element.maxAmp + '" disabled="disabled">'+
+		'</div>'+
+    '</div>'+
     '</div>' +
     '<div>' +
     '<a href="#" pmedittarget="' + element.dIO + '" class="ui-btn ui-shadow ui-corner-all ui-icon-edit ui-btn-icon-notext ui-btn-inline">Edit</a>' +
     '<a href="#" pmdeletetarget="' + element.dIO + '" class="ui-btn ui-shadow ui-corner-all ui-icon-delete ui-btn-icon-notext ui-btn-inline">Delete</a>' +
-    '</div>';
+    '</div>'+
+    '</form>';
 }
 
 function _removePowermeterDisplay(dIO) {
@@ -329,7 +361,7 @@ $(document).ready(function () {
     }
     // MQTT connection status
     if (pmdEvent.type === "msc") {
-      $("#HA_status").html(pmdEvent.datas ? "connected" : "disconnected");
+      $("#mqttstatus").val(pmdEvent.datas ? "connected" : "disconnected");
 
     }
   };
