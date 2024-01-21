93f8197de99412234da0be57290e5dcbba6d00c6
Stephane Blanc <sblanckila@gmail.com>
2024-01-21 20:23:49 +0100

    add build index

--- src/Powermeter.hpp
+++ src/Powermeter.hpp
@@ -11,16 +11,16 @@ struct PowermeterDef
     int voltage = 0;
     int maxAmp = 0;
 };
-static const uint8_t PM1 = D3;
-static const uint8_t PM2 = D2;
-static const uint8_t PM3 = D1;
-static const uint8_t PM4 = D5;
-static const uint8_t PM5 = D6;
-static const uint8_t PM6 = D7;
-static const uint8_t PM7 = D8;
-static const uint8_t PM8 = D9;
-static const uint8_t PM9 = D10;
-static const uint8_t PM10 = D4;
+static const uint8_t PM1 = D3; //0
+static const uint8_t PM2 = D2; //4
+static const uint8_t PM3 = D1; //5
+static const uint8_t PM4 = D5; //14
+static const uint8_t PM5 = D6; //12
+static const uint8_t PM6 = D7; //13
+static const uint8_t PM7 = D8; //15
+static const uint8_t PM8 = D9; //3
+static const uint8_t PM9 = D10; //1
+static const uint8_t PM10 = D4; //2
 static const uint8_t PMB_LED = D0;
 static const uint8_t PowermeterIndexToBoardIO[10] = {PM1, PM2, PM3, PM4, PM5, PM6, PM7, PM8, PM9, PM10};
 static const uint8_t BoardIOToPowermeterIndex[16] = {0,8,9,7,1,2,255,255,255,255,255,255,4,5,3,6};
@@ -28,7 +28,7 @@ static const uint8_t BoardIOToPowermeterIndex[16] = {0,8,9,7,1,2,255,255,255,255
 class Powermeter
 {
 public:
-    Powermeter(PowermeterDef p_Definition, DDS238Data p_values, HADevice *p_pHADevice, HA_DDS238_PERSISTENT_FUNCTION p_PersistenceFunction)
+    Powermeter(PowermeterDef p_Definition, DDS238Data p_Values, HADevice *p_pHADevice, HA_DDS238_PERSISTENT_FUNCTION p_PersistenceFunction)
     {
         // keep powermeter definition
         m_Definition = p_Definition;
@@ -43,7 +43,8 @@ public:
             p_PersistenceFunction);
 
         // setup HomeAssistant Adapter
-        m_pPowermeterAdapter->restore(p_values);
+        m_pPowermeterAdapter->restore(p_Values);
+        
         m_pPowermeterAdapter->setup();
 
         // link HomeAssistant Adapter to device
